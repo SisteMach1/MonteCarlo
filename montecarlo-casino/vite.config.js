deb98565ada6743dbb8434fad12f442df14dd6bb
SisteMach <maxiisanchezz2020@gmail.com>
2026-09-08 01:11:54 -0300

    Delete montecarlo-casino directory

--- montecarlo-casino/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANTE: Cambiá 'montecarlo-casino' por el nombre de TU repo en GitHub
-// Ejemplo: si tu repo es https://github.com/tuusuario/mi-casino -> poné base: '/mi-casino/'
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Esto hace que funcione en cualquier repo y en GitHub Pages
-})
